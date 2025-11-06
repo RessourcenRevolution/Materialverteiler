19321dfeab79e380662422d18ffdfea48bbdee74
Arco Mul <arco.mul@denkwerk.com>
2025-11-06 13:55:33 +0100

    fix(ui): export components in package.json

---
+++ packages/ui/components/index.ts
@@ -0,0 +1,18 @@
+export {
+  default as Alert,
+  type Props as AlertProps,
+} from "./alert/Alert.astro";
+export {
+  default as Button,
+  type Props as ButtonProps,
+} from "./button/Button.astro";
+export { default as Card, type Props as CardProps } from "./card/Card.astro";
+export {
+  default as Input,
+  type Props as InputProps,
+} from "./input/Input.astro";
+export { default as Link, type Props as LinkProps } from "./link/Link.astro";
+export {
+  default as Textarea,
+  type Props as TextareaProps,
+} from "./textarea/Textarea.astro";
