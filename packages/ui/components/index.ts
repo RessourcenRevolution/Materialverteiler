56ec82efc77436eab433ddc05f915130fc768271
Arco Mul <arco.mul@denkwerk.com>
2025-11-13 16:14:12 +0100

    feat(ui): add ImageInput component

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -12,6 +12,10 @@ export {
   type Props as ContainerProps,
 } from "./container/Container.astro";
 export {
+  default as ImageInput,
+  type Props as ImageInputProps,
+} from "./image-input/ImageInput.astro";
+export {
   default as Input,
   type Props as InputProps,
 } from "./input/Input.astro";
