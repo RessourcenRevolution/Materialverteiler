a7a505d7ee04ea5df37931e9cf1b6fc3b7fda1d6
Arco Mul <arco.mul@denkwerk.com>
2025-11-20 10:34:13 +0100

    feat: add first tailwind theme values + fluid font-sizes

---
+++ packages/ui/components/image-input/ImageInput.stories.ts
@@ -0,0 +1,14 @@
+import ImageInput, { type Props } from "./ImageInput.astro";
+import FlexDecorator from "../../.astrobook/decorators/FlexDecorator.astro";
+
+export default {
+  component: ImageInput,
+};
+
+export const Default = {
+  args: {
+    name: "input",
+    label: "Label",
+  } satisfies Props,
+  decorators: [{ component: FlexDecorator }],
+};
