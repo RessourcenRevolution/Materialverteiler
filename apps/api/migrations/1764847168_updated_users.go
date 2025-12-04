5cf13deab6d5a160b07534bbfcfc19411ef364c0
Arco Mul <arco.mul@denkwerk.com>
2025-12-04 12:24:04 +0100

    feat(api): add 'terms' field to users

---
+++ apps/api/migrations/1764847168_updated_users.go
@@ -0,0 +1,40 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(11, []byte(`{
+			"hidden": false,
+			"id": "bool2292334449",
+			"name": "terms",
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "bool"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("bool2292334449")
+
+		return app.Save(collection)
+	})
+}
