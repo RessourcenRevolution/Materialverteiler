a8897da24a7fb37d129e663b721c8fc19a87753f
Arco Mul <arco.mul@denkwerk.com>
2025-12-18 12:32:35 +0100

    fix: actually logout when logging out

--- apps/web/src/pages/app/logout.ts
+++ apps/web/src/pages/app/logout.ts
@@ -3,6 +3,17 @@ import type { APIRoute } from 'astro'
 export const prerender = false
 
 export const GET: APIRoute = async ({ locals, redirect }) => {
+  // Clear pocketbase auth state
   locals.pb.authStore.clear()
-  return redirect('/app/login')
+
+  // Get redirect response
+  const response = redirect('/app/login')
+
+  // Append new empty cookie
+  const authCookie = locals.pb.authStore.exportToCookie({
+    sameSite: 'lax',
+  })
+  response.headers.append('set-cookie', authCookie)
+
+  return response
 }
