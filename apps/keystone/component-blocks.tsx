2dfe757c42d734bf91068ca9f7a4049e6e8b47fb
Arco Mul <arco.mul@denkwerk.com>
2026-01-08 15:12:55 +0100

    feat: add Text as keystone component + align modules better on keystone pages

--- apps/keystone/component-blocks.tsx
+++ apps/keystone/component-blocks.tsx
@@ -28,4 +28,48 @@ export const componentBlocks = {
       );
     },
   }),
+  text: component({
+    label: "Text",
+    schema: {
+      size: fields.select({
+        label: "Size",
+        options: [
+          { label: "Normal", value: "normal" },
+          { label: "Large", value: "large" },
+        ],
+        defaultValue: "normal",
+      }),
+      framed: fields.checkbox({
+        label: "Framed",
+      }),
+      content: fields.child({
+        kind: "block",
+        placeholder: "Enter text...",
+        formatting: {
+          inlineMarks: "inherit",
+          softBreaks: "inherit",
+          headingLevels: "inherit",
+        },
+        links: "inherit",
+      }),
+    },
+    preview: (props) => {
+      const framed = props.fields.framed.value;
+      return (
+        <p
+          style={{
+            fontSize: props.fields.size.value === "large" ? "1.25rem" : "1rem",
+            padding: framed ? "1rem" : "0",
+            background: framed
+              ? "linear-gradient(to right, black 3px, transparent 3px) 0 0, linear-gradient(to bottom, black 3px, transparent 3px) 0 0,linear-gradient(to left, black 3px, transparent 3px) 100% 100%, linear-gradient(to top, black 3px, transparent 3px) 100% 100%"
+              : "",
+            backgroundSize: "1.25rem 1.25rem",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          {props.fields.content.element}
+        </p>
+      );
+    },
+  }),
 };
