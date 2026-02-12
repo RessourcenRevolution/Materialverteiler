ef2d04ffb67d3f6503590af1b692281f5aaed8a2
Arco Mul <arco.mul@denkwerk.com>
2026-02-12 14:42:43 +0100

    feat: add 512x512 thumbs for listing images

---
+++ apps/api/migrations/1770903569_updated_listings.go
@@ -0,0 +1,65 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3660939671")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(7, []byte(`{
+			"hidden": false,
+			"id": "file3760176746",
+			"maxSelect": 99,
+			"maxSize": 0,
+			"mimeTypes": [],
+			"name": "images",
+			"presentable": false,
+			"protected": false,
+			"required": false,
+			"system": false,
+			"thumbs": [
+				"512x512f",
+				"1024x1024f"
+			],
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3660939671")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(7, []byte(`{
+			"hidden": false,
+			"id": "file3760176746",
+			"maxSelect": 99,
+			"maxSize": 0,
+			"mimeTypes": [],
+			"name": "images",
+			"presentable": false,
+			"protected": false,
+			"required": false,
+			"system": false,
+			"thumbs": [
+				"1024x1024f"
+			],
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
