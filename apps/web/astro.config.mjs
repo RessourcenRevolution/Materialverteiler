06c1fb652f36af7d06f5b3e73ef985f2c719c666
Arco Mul <arco.mul@denkwerk.com>
2026-04-15 17:14:44 +0200

    fix: define APP_HOST as allowedDomains in astro config

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig, envField } from 'astro/config'
 import node from '@astrojs/node'
 import tailwindcss from '@tailwindcss/vite'
-// @ts-ignore
+// @ts-expect-error Lit has no types
 import lit from '@semantic-ui/astro-lit'
 
-// https://astro.build/config
+// https://astro.build/confi
 export default defineConfig({
   server: {
     port: Number(process.env.APP_PORT) || 3000,
+  },
+
+  security: {
+    checkOrigin: true,
+    allowedDomains: [{ hostname: process.env.APP_HOST }],
   },
 
   adapter: node({
